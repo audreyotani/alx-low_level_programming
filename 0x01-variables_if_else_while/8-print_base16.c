e9a52eefffec0b6713c2b25010d1062ab3e35fd1
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-09 08:09:02 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'hexadecimal numbers'
+ * Return: always (0) Success
+ */
+
+int main(void)
+{
+	int num = 0
+	char letter = 'a'
+
+	while (num < 10)
+	{
+		putchar((num % 10) + '0');
+		num++;
+	}
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
