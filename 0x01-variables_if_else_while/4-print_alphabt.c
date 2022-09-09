f242633a22d8855c7c16d15a9871d3b313e72ec9
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-09 04:43:19 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'All alphabets except'
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
