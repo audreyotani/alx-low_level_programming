cba9643fbbdece566c5c1c1a55756f30dc5e4bff
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-09 08:23:09 -0700

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Single digits in asecnding order'
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int single = 0;
+
+	while (single <= 9)
+	{
+		putchar((single % 10) + '0');
+		if (num == 9)
+		continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
