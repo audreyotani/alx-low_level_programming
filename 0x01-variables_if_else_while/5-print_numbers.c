eae6a5cb0074a8b756db849f6af2b8c2c9c9fa3d
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-09 04:53:34 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'baseten numbers'
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 10)
+	{
+		printf("%d\n", num);
+		num++;
+	}
+	return (0);
+}
