3c643d8e0ae66f42e3c2fdb4468e00527d7c7789
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-13 10:44:28 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - prototype
+ * @num1: First integer
+ * @num2: Second integer
+ * Description: 'Prints result of sum'
+ * Return: Sum of two integers
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
