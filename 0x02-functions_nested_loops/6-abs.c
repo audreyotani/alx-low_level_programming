30e1982343ac68c2dab26bac5d9731742b74641e
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-13 10:23:15 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - prototype
+ * Description: 'Absolute value'
+ * @n: integer
+ * Return: Always (n) success
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+}
