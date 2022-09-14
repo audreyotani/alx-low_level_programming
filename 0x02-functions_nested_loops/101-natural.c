8ff6d33963ba074a4fee721e86b13c7ce80a1073
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-14 01:52:54 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,14 @@
 
 int main(void)
 {
-	int sum = 0;
+	int sum;
 	int i;
+
+	sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			sum += i;
 		}
