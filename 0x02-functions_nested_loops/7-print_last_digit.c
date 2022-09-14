0a2525608d60a09628a4f192baa85511f0597a42
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-14 04:03:11 -0700

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,13 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		n *= -1;
-	}
-	_putchar((n % 10) + '0');
+	int digit = n % 10;
 
-	return (n % 10);
+	if (digit < 0)
+	{
+		digit *= -1;
+	}
+	_putchar(digit + '0');
+
+	return (digit);
 }
