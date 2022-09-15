6e47cfdf81a06cd6bb39bf8d2c032bf176f1de3a
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-15 09:19:04 -0700

    pipe

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	long x, max;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			max = number / x;
+		}
+	}
+	printf("%ld\n", max);
+
+	return (0);
+}
