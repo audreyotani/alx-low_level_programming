c9bc9d313d5e3fa0ff2b87ca82ddb805d121d2e7
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-15 00:53:44 -0700

    most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prototype
+ * Return: return value
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+	}
+	_putchar('\n');
+}
