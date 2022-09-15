3fc21fb263c68af7c89117dd183aa90b12cc6b99
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-15 04:38:11 -0700

    largest

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * largest_number - prototype
+ * @a: Integer
+ * @b: Integer
+ * @c: Integer
+ * Return: largest
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
