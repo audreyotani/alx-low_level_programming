7e1928b66974b0cd11a92815026800dacf4bb9b1
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-15 09:25:42 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prototype
+ * @n: Integer
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	else if (n / 10)
+	{
+		_putchar((n % 10) + '0');
+	}
+}
