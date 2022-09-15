b32add3b55c4b5b995fc85835f90a95851aebcd8
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-15 03:46:59 -0700

    printline

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,9 @@ void print_line(int n)
 {
 	int a;
 
-	if (n <= 0)
+	for (a = 0; a <= n; a++)
 	{
-		_putchar('\n');
+		_putchar('_');
 	}
-	else
-	{
-		for (a = 0; a <= n; a++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
