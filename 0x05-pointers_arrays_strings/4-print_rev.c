eb87eb6d24288e0120da27168cbbeafe8ef596db
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-19 05:58:02 -0700

    printrev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prototype
+ * @s: charcater
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	for (a = -1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
