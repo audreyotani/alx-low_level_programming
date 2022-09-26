42febaaad756d024f8fc475d3b6af41a9cade945
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-26 05:01:42 -0700

    locate

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - prototype to locate character in a string
+ * @s: string
+ * @c: character
+ * Return: pointer to c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
