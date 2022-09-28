8f7380c65c0b1ade26920299db22818cc07dd186
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-28 03:47:24 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - prototype to return natural square root
+ * @n: integer to return square root
+ * Return: square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_recursion(n - 1));
+	}
+}
