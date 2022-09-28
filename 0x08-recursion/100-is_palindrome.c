ff9ab1d9a6d670318bd48beca5079101485219d7
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-28 04:06:50 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_palindrome - prototype to print a palindrome
+ * @s: string to be checked
+ * Return: 1
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
