c4d850a7e8419dee10881b2c2ada53e4e62b8ccf
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-09-30 05:07:45 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: function to count arguments
+ * @argv: array of points to strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
