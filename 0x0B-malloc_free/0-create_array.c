68808cefb17d5dbd149f84c0b301a089bbcf6d31
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-10-04 16:14:12 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,10 +22,10 @@ char *create_array(unsigned int size, char c)
 	if (p == NULL)
 	{
 		return (NULL);
-		for (i = 0; i < size; i++)
-		{
-			p[i] = c;
-		}
+	}
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
 	}
 	return (p);
 }
