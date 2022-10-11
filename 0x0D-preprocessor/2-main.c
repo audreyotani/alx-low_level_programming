22081d26de59dedfc943d55f6e3ad12ae7f46185
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-10-11 11:37:51 -0700

    pull

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 success
+ * @argc: argument count
+ * @argv: argument value
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
