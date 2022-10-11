a283c8cc0a8c37fcdf646d19e76077751c0fbac5
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-10-11 03:59:47 -0700

    initialize

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - prototype to initialize structure
+ * @d: structure type
+ * @age: float
+ * @name: character
+ * @owner: character
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
