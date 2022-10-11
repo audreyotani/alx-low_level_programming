701ed8e8ba4a957816417fc339cc8c6f332e7376
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-10-11 10:59:30 -0700

    fih

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - new type
- * @name: character string
- * @age: integer
- * @owner: character string
- */
 struct dog
 {
 	char *name;
@@ -14,3 +8,6 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+
+#endif
