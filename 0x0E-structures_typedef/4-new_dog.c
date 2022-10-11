14675edf2802f698f0d355fdf1db0098d37d3e1b
Audrey Otani <nicoleaudreyotani@gmail.com>
2022-10-11 11:12:39 -0700

    ll

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**new_dog - prototype
+ * @name: char string
+ * @age: integer
+ * @owner: char string
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
