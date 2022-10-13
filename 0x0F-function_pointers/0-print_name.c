299e2fbd9d1e601cabe3ed43e2d53468a8d5b040
audreyotani <nicoleaudreyotani@gmail.com>
2022-10-13 06:18:42 -0700

    printname

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prototype to print name
@@ -8,5 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL && f != NULL)
+
 	f(name);
 }
