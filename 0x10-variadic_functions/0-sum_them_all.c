8641b48c1f2cf03b934c23e0c4861edaa820fafd
audreyotani <nicoleaudreyotani@gmail.com>
2022-10-14 10:26:47 -0700

    sums

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - prototype
+ * @n: Constant integer
+ * Return: sum of parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list list;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
+}
